934b3671eefb61ce58a5996686900881a5eb18b2
Lloyd <lloydmeta@gmail.com>
2016-04-16 02:37:26 +0900

    1.4.0

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 
 object Enumeratum extends Build {
 
-  lazy val theVersion = "1.4.0-SNAPSHOT"
+  lazy val theVersion = "1.4.0"
   lazy val theScalaVersion = "2.11.8"
   lazy val scalaVersions = Seq("2.10.6", "2.11.8")
   def thePlayVersion(scalaVersion: String) = CrossVersion.partialVersion(scalaVersion) match {
