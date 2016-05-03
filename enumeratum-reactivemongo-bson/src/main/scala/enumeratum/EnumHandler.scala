940f38d72d8c13c0b6128be72339d4d085fb81c9
Alessandro Lacava <alessandrolacava@gmail.com>
2016-05-03 09:54:41 +0200

    add stable val reader and writer to the handler

--- enumeratum-reactivemongo-bson/src/main/scala/enumeratum/EnumHandler.scala
+++ enumeratum-reactivemongo-bson/src/main/scala/enumeratum/EnumHandler.scala
@@ -11,32 +11,30 @@ import scala.util.{ Failure, Success }
  * @since 2016-04-23
  */
 object EnumHandler {
-
   /**
    * Returns a BSONReader for a given enum [[Enum]]
    *
    * @param enum The enum
    * @param insensitive bind in a case-insensitive way, defaults to false
    */
-  def reader[A <: EnumEntry]
-    (enum: Enum[A], insensitive: Boolean = false): BSONReader[BSONValue, A] = new BSONReader[BSONValue, A] {
-
-    override def read(bson: BSONValue): A = {
-      val result = bson match {
-        case BSONString(s) => {
-          val maybeBound = if (insensitive) enum.withNameInsensitiveOption(s) else enum.withNameOption(s)
-          maybeBound match {
-            case Some(obj) => Success(obj)
-            case None => Failure(new RuntimeException(
-              s"Enumeration expected of type: '$enum', but it does not appear to contain the value: '$s'"
-            ))
+  def reader[A <: EnumEntry](enum: Enum[A], insensitive: Boolean = false): BSONReader[BSONValue, A] =
+    new BSONReader[BSONValue, A] {
+      override def read(bson: BSONValue): A = {
+        val result = bson match {
+          case BSONString(s) => {
+            val maybeBound = if (insensitive) enum.withNameInsensitiveOption(s) else enum.withNameOption(s)
+            maybeBound match {
+              case Some(obj) => Success(obj)
+              case None => Failure(new RuntimeException(
+                s"Enumeration expected of type: '$enum', but it does not appear to contain the value: '$s'"
+              ))
+            }
           }
+          case _ => Failure(new RuntimeException("String value expected"))
         }
-        case _ => Failure(new RuntimeException("String value expected"))
-      }
 
-      result.get
-    }
+        result.get
+      }
   }
 
   /**
@@ -52,12 +50,13 @@ object EnumHandler {
    * @param enum The enum
    * @param insensitive bind in a case-insensitive way, defaults to false
    */
-  def handler[A <: EnumEntry]
-    (enum: Enum[A], insensitive: Boolean = false): BSONHandler[BSONValue, A] = new BSONHandler[BSONValue, A] {
+  def handler[A <: EnumEntry](enum: Enum[A], insensitive: Boolean = false): BSONHandler[BSONValue, A] =
+    new BSONHandler[BSONValue, A] {
+      val concreteReader = reader(enum, insensitive)
+      val concreteWriter = writer(enum)
 
-    override def read(bson: BSONValue): A = reader(enum).read(bson)
+      override def read(bson: BSONValue): A = concreteReader.read(bson)
 
-    override def write(t: A): BSONValue = writer(enum).write(t)
+      override def write(t: A): BSONValue = concreteWriter.write(t)
   }
-
 }
