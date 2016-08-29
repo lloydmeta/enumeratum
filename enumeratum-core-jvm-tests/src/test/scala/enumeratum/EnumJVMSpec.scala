f4c5d525119b1687dd8e4e99cc475b4038b0203b
Lloyd <lloydmeta@gmail.com>
2016-08-30 02:18:37 +0900

    Add non-deterministic tests for ValueEnums

--- enumeratum-core-jvm-tests/src/test/scala/enumeratum/EnumJVMSpec.scala
+++ enumeratum-core-jvm-tests/src/test/scala/enumeratum/EnumJVMSpec.scala
@@ -9,7 +9,7 @@ class EnumJVMSpec extends FunSpec with Matchers {
     // This is a fairly intense test.
     it("should be in the same order that the objects were declared in") {
       import scala.util._
-      (1 to 100).foreach { i =>
+      (1 to 50).foreach { i =>
         val members = Random.shuffle((1 to Random.nextInt(20)).map { m => s"Member$m" })
         val membersDefs = members.map { m => s"case object $m extends Enum$i" }.mkString("\n\n")
         val objDefinition =
---
+++ enumeratum-core-jvm-tests/src/test/scala/enumeratum/values/ValueEnumJVMSpec.scala
@@ -0,0 +1,65 @@
+package enumeratum.values
+
+import enumeratum.Eval
+import org.scalatest.{ FunSpec, Matchers }
+
+import scala.reflect.ClassTag
+import scala.util.Random
+
+/**
+ * Created by Lloyd on 8/30/16.
+ *
+ * Copyright 2016
+ */
+class ValueEnumJVMSpec extends FunSpec with Matchers {
+
+  /*
+   Non-deterministically generates a bunch of different types of ValueEnums and tests the ability to resolve
+   proper members by value
+    */
+  testValuesOf(Stream.continually(Random.nextInt()))
+  testValuesOf(Stream.continually(Random.nextLong()), valueSuffix = "L")
+  testValuesOf(Stream.continually(Random.nextInt()).collect { case i if i >= Short.MinValue && i <= Short.MaxValue => i.toShort })
+  testValuesOf(stringGenerator, "\"", "\"")
+
+  private def stringGenerator = Random.alphanumeric.grouped(10).toStream.map(_.mkString.replaceAll("[0-9]", "")).distinct
+
+  private def testValuesOf[A: ClassTag](generator: Stream[A], valuePrefix: String = "", valueSuffix: String = ""): Unit = {
+
+    val typeName = implicitly[ClassTag[A]].runtimeClass.getSimpleName.capitalize
+
+    describe(s"${typeName}Enum generation tests") {
+
+      it("should create a value enum that can properly resolve values to members") {
+        (1 to 10).foreach { i =>
+          val enumName = s"Generated${typeName}Enum$i"
+          val names = stringGenerator.take(5)
+          val values = generator.distinct
+          val namesToValues = names.zip(values)
+          val memberDefs = namesToValues.map { case (n, v) => s"""case object $n extends $enumName($valuePrefix$v$valueSuffix)""" }.mkString("\n\n")
+          val objDef =
+            s"""
+               |import enumeratum.values._
+               |
+          |sealed abstract class $enumName(val value: $typeName) extends ${typeName}EnumEntry
+               |
+          |object $enumName extends ${typeName}Enum[$enumName] {
+               |  val values = findValues
+               |
+          |  $memberDefs
+               |}
+               |$enumName
+        """.stripMargin
+          val obj = Eval.apply[ValueEnum[A, _ <: ValueEnumEntry[A]]](objDef)
+          namesToValues.foreach {
+            case (n, v) =>
+              obj.withValue(v).toString shouldBe n
+          }
+        }
+      }
+
+    }
+
+  }
+
+}
