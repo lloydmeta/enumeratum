fe7bb92f98c69c3c1f84e668bdd03bf475ed796a
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2016-10-15 00:07:44 +0100

    Complete the test cases.

--- enumeratum-argonaut/src/test/scala/enumeratum/ArgonautSpec.scala
+++ enumeratum-argonaut/src/test/scala/enumeratum/ArgonautSpec.scala
@@ -16,6 +16,48 @@ class ArgonautSpec extends FunSpec with Matchers {
         value.asJson shouldBe value.entryName.asJson
       }
     }
+
+    it("should work for lower case") {
+      TrafficLight.values.foreach { value =>
+        value.asJson(Argonauter.encoderLowercase(TrafficLight)) shouldBe value.entryName.toLowerCase.asJson
+      }
+    }
+
+    it("should work for upper case") {
+      TrafficLight.values.foreach { value =>
+        value.asJson(Argonauter.encoderUppercase(TrafficLight)) shouldBe value.entryName.toUpperCase.asJson
+      }
+    }
+  }
+
+  describe("from JSON") {
+    it("should parse enum members when given proper encoding") {
+      TrafficLight.values.foreach { value =>
+        value.entryName.asJson.as[TrafficLight] shouldBe okResult(value)
+      }
+    }
+
+    it("should parse enum members when given proper encoding for lower case") {
+      TrafficLight.values.foreach { value =>
+        value.entryName.toLowerCase.asJson
+          .as[TrafficLight](Argonauter.decoderLowercaseOnly(TrafficLight)) shouldBe okResult(value)
+      }
+    }
+
+    it("should parse enum members when given proper encoding for upper case") {
+      TrafficLight.values.foreach { value =>
+        value.entryName.toUpperCase.asJson
+          .as[TrafficLight](Argonauter.decoderUppercaseOnly(TrafficLight)) shouldBe okResult(value)
+      }
+    }
+
+    it("should fail to parse random JSON values to members") {
+      val results = Seq("XXL".asJson, Int.MaxValue.asJson).map(_.as[TrafficLight])
+      results.foreach { res =>
+        res.result.isLeft shouldBe true
+        res.history.map(_.toList) shouldBe Some(Nil)
+      }
+    }
   }
 
 }
