a72c138b7b287f2cc13456171c3b05fbb459c607
Lloyd <lloydmeta@gmail.com>
2016-11-10 15:57:47 +0900

    Add a comment to track  https://github.com/sbt/sbt/issues/2786 or https://github.com/olafurpg/scalafmt/issues/485 to know when we can remove the MediatorWorkaround plugin

--- project/MediatorWorkaround.scala
+++ project/MediatorWorkaround.scala
@@ -1,6 +1,8 @@
 import sbt._
 import Keys._
 
+// Track https://github.com/sbt/sbt/issues/2786 or https://github.com/olafurpg/scalafmt/issues/485
+// to know when we can remove this workaround
 object MediatorWorkaround extends AutoPlugin {
   override def requires = plugins.JvmPlugin
   override def trigger = allRequirements
