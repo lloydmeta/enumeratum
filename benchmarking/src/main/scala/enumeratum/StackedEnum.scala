c53ae61c3c6534e6b9ff3009e1009a91b14721e0
Lloyd <lloydmeta@users.noreply.github.com>
2017-02-05 13:21:07 +0900

    - Add benchmarks for entryName (#106)
    - Bump sbt-jmh version

---
+++ benchmarking/src/main/scala/enumeratum/StackedEnum.scala
@@ -0,0 +1,20 @@
+package enumeratum
+
+import enumeratum.EnumEntry.UpperHyphencase
+
+import scala.collection.immutable.IndexedSeq
+
+/**
+  * Created by Lloyd on 2/5/17.
+  *
+  * Copyright 2017
+  */
+sealed abstract class StackedEnum extends EnumEntry with UpperHyphencase
+
+case object StackedEnum extends Enum[StackedEnum] {
+
+  val values: IndexedSeq[StackedEnum] = findValues
+
+  case object SomethingRedAndSmall extends StackedEnum
+  case object SomethingBlueAndBig  extends StackedEnum
+}
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -12,7 +12,7 @@ addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "0.8.5")
 
 addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.14")
 
-addSbtPlugin("pl.project13.scala" % "sbt-jmh" % "0.2.18")
+addSbtPlugin("pl.project13.scala" % "sbt-jmh" % "0.2.21")
 
 addSbtPlugin("org.wartremover" % "sbt-wartremover" % "1.2.1")
 
