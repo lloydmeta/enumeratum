91426aced93401dfb9c82c74ac97134b3d76d215
Lloyd <lloydmeta@users.noreply.github.com>
2017-03-14 17:52:36 +0900

    Tests/json4s doctests (#126)
    * Add doctests for Json4s.serialiser
    
    * Add what I can for value enum serialiser generators

--- enumeratum-json4s/src/main/scala/enumeratum/Json4s.scala
+++ enumeratum-json4s/src/main/scala/enumeratum/Json4s.scala
@@ -6,6 +6,33 @@ import org.json4s.JsonAST.JString
 @SuppressWarnings(Array("org.wartremover.warts.Any"))
 object Json4s {
 
+  /**
+    * Returns a Json [[CustomSerializer]] for the given Enumeratum enum
+    *
+    * {{{
+    * scala> import enumeratum._
+    * scala> import org.json4s._
+    * scala> import org.json4s.native.Serialization
+    *
+    * scala> sealed trait ShirtSize extends EnumEntry
+    * scala> case object ShirtSize extends Enum[ShirtSize] {
+    *      |  case object Small  extends ShirtSize
+    *      |  case object Medium extends ShirtSize
+    *      |  case object Large  extends ShirtSize
+    *      |  val values = findValues
+    *      | }
+    *
+    * scala> implicit val formats = Serialization.formats(NoTypeHints) + Json4s.serializer(ShirtSize)
+    *
+    * scala> Serialization.write(ShirtSize.values)
+    * res0: String = ["Small","Medium","Large"]
+    *
+    * scala> Serialization.read[Seq[ShirtSize]]("""["Small","Medium","Large"]""") == ShirtSize.values
+    * res1: Boolean = true
+    * }}}
+    *
+    * @param enum the enum you want to generate a Json4s serialiser for
+    */
   def serializer[A <: EnumEntry: Manifest](enum: Enum[A]): CustomSerializer[A] =
     new CustomSerializer[A](
       _ =>
