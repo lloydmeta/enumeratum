71b255fe8677df16a1d5584f692a68374d2fde18
Lloyd <lloydmeta@gmail.com>
2017-03-18 18:39:17 +0900

    Core 1.5.9 release

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,8 +1,8 @@
 object Versions {
 
   object Core {
-    val stable = "1.5.8"
-    val head   = "1.5.9-SNAPSHOT"
+    val stable = "1.5.9"
+    val head   = "1.5.10-SNAPSHOT"
   }
 
   object Macros {
