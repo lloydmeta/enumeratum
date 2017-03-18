e2251c896ece7dfea75741338dfbc02d25484281
Lloyd <lloydmeta@users.noreply.github.com>
2017-03-18 20:25:29 +0900

    Release/macro 1.5.8 (#130)
    * Macros 1.5.8 release
    
    * Macros 1.5.9-SNAPSHOT begins
    
    * Core 1.5.9 release
    
    * Core 1.5.10-SNAPSHOT begins
    
    * Release non-snapshot versions of integration libs
    
    * SNAPSHOT versions of integration libs begin

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,13 +1,13 @@
 object Versions {
 
   object Core {
-    val stable = "1.5.8"
-    val head   = "1.5.9-SNAPSHOT"
+    val stable = "1.5.9"
+    val head   = "1.5.10-SNAPSHOT"
   }
 
   object Macros {
-    val stable = "1.5.7"
-    val head   = "1.5.8-SNAPSHOT"
+    val stable = "1.5.8"
+    val head   = "1.5.9-SNAPSHOT"
   }
 
 }
