80c055c31505a75496097fac91403a177da8d005
Lloyd <lloydmeta@gmail.com>
2017-03-21 13:48:32 +0900

    Core v1.5.10 release

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,8 +1,8 @@
 object Versions {
 
   object Core {
-    val stable = "1.5.9"
-    val head   = "1.5.10-SNAPSHOT"
+    val stable = "1.5.10"
+    val head   = "1.5.11-SNAPSHOT"
   }
 
   object Macros {
