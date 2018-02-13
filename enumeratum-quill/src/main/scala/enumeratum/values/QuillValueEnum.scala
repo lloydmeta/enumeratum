77d51f6b20d7f54ad5f55efdf4d49a9cc59c8435
Daniel Shuy <daniel_shuy@hotmail.com>
2018-02-13 18:04:42 +0800

    Encode/Decode CharEnum from/to String instead

--- enumeratum-quill/src/main/scala/enumeratum/values/QuillValueEnum.scala
+++ enumeratum-quill/src/main/scala/enumeratum/values/QuillValueEnum.scala
@@ -2,18 +2,18 @@ package enumeratum.values
 
 import io.getquill.MappedEncoding
 
-sealed trait QuillValueEnum[ValueType, EntryType <: ValueEnumEntry[ValueType]] {
+sealed trait QuillValueEnum[ValueType, EntryType <: ValueEnumEntry[ValueType], QuillType] {
   this: ValueEnum[ValueType, EntryType] =>
 
   /**
     * Implicit Encoder for this enum
     */
-  implicit val quillEncoder: MappedEncoding[EntryType, ValueType] = Quill.encoder(this)
+  implicit val quillEncoder: MappedEncoding[EntryType, QuillType]
 
   /**
     * Implicit Decoder for this enum
     */
-  implicit val quillDecoder: MappedEncoding[ValueType, EntryType] = Quill.decoder(this)
+  implicit val quillDecoder: MappedEncoding[QuillType, EntryType]
 }
 
 /**
@@ -42,41 +42,61 @@ sealed trait QuillValueEnum[ValueType, EntryType <: ValueEnumEntry[ValueType]] {
   * res0: String = INSERT INTO Shirt (size) VALUES (?)
   * }}}
   */
-trait IntQuillEnum[EntryType <: IntEnumEntry] extends QuillValueEnum[Int, EntryType] {
+trait IntQuillEnum[EntryType <: IntEnumEntry] extends QuillValueEnum[Int, EntryType, Int] {
   this: ValueEnum[Int, EntryType] =>
+  implicit val quillEncoder: MappedEncoding[EntryType, Int] = Quill.encoder(this)
+  implicit val quillDecoder: MappedEncoding[Int, EntryType] = Quill.decoder(this)
 }
 
 /**
   * QuillEnum for LongEnumEntry
   */
-trait LongQuillEnum[EntryType <: LongEnumEntry] extends QuillValueEnum[Long, EntryType] {
+trait LongQuillEnum[EntryType <: LongEnumEntry] extends QuillValueEnum[Long, EntryType, Long] {
   this: ValueEnum[Long, EntryType] =>
+  implicit val quillEncoder: MappedEncoding[EntryType, Long] = Quill.encoder(this)
+  implicit val quillDecoder: MappedEncoding[Long, EntryType] = Quill.decoder(this)
 }
 
 /**
   * QuillEnum for ShortEnumEntry
   */
-trait ShortQuillEnum[EntryType <: ShortEnumEntry] extends QuillValueEnum[Short, EntryType] {
+trait ShortQuillEnum[EntryType <: ShortEnumEntry] extends QuillValueEnum[Short, EntryType, Short] {
   this: ValueEnum[Short, EntryType] =>
+  implicit val quillEncoder: MappedEncoding[EntryType, Short] = Quill.encoder(this)
+  implicit val quillDecoder: MappedEncoding[Short, EntryType] = Quill.decoder(this)
 }
 
 /**
   * QuillEnum for StringEnumEntry
   */
-trait StringQuillEnum[EntryType <: StringEnumEntry] extends QuillValueEnum[String, EntryType] {
+trait StringQuillEnum[EntryType <: StringEnumEntry] extends QuillValueEnum[String, EntryType, String] {
   this: ValueEnum[String, EntryType] =>
+  implicit val quillEncoder: MappedEncoding[EntryType, String] = Quill.encoder(this)
+  implicit val quillDecoder: MappedEncoding[String, EntryType] = Quill.decoder(this)
 }
 
 /**
   * QuillEnum for CharEnumEntry
   */
-trait CharQuillEnum[EntryType <: CharEnumEntry] extends QuillValueEnum[Char, EntryType] {
+trait CharQuillEnum[EntryType <: CharEnumEntry] extends QuillValueEnum[Char, EntryType, String] {
   this: ValueEnum[Char, EntryType] =>
+
+  /**
+    * Because all existing Quill contexts do not have built-in Encoders for Char, convert it to a String instead.
+    */
+  implicit val quillEncoder: MappedEncoding[EntryType, String] = MappedEncoding(enum => String.valueOf(enum.value))
+
+  /**
+    * Because all existing Quill contexts do not have built-in Decoders for Char, convert it from a String instead.
+    */
+  implicit val quillDecoder: MappedEncoding[String, EntryType] = MappedEncoding(str => withValue(str.charAt(0)))
 }
 
 /**
   * QuillEnum for ByteEnumEntry
   */
-trait ByteQuillEnum[EntryType <: ByteEnumEntry] extends QuillValueEnum[Byte, EntryType] {
+trait ByteQuillEnum[EntryType <: ByteEnumEntry] extends QuillValueEnum[Byte, EntryType, Byte] {
   this: ValueEnum[Byte, EntryType] =>
+  implicit val quillEncoder: MappedEncoding[EntryType, Byte] = Quill.encoder(this)
+  implicit val quillDecoder: MappedEncoding[Byte, EntryType] = Quill.decoder(this)
 }
