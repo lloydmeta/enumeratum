3a171313cd92551446bfb41e0e9035af7efbc6f7
Yannick Heiber <yannick.heiber@free2move.me>
2018-10-01 15:38:14 +0200

    Simplify showInstance in CatsEnum

--- enumeratum-cats/src/main/scala/enumeratum/CatsEnum.scala
+++ enumeratum-cats/src/main/scala/enumeratum/CatsEnum.scala
@@ -7,7 +7,7 @@ trait CatsEnum[A <: EnumEntry] { this: Enum[A] =>
 
   implicit val eqInstance: Eq[A] = Eq.fromUniversalEquals[A]
 
-  implicit val showInstance: Show[A] = Show.fromToString[A]
+  implicit val showInstance: Show[A] = Show.show(_.entryName)
 
   implicit val hashInstance: Hash[A] = Hash.by(_.entryName)
 
