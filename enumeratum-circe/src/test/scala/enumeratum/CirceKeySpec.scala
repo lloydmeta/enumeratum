a85a8678459acba2a76bb900d77cfebc73ada9b7
Yan Su <yaroot@users.noreply.github.com>
2019-03-21 20:50:49 +0900

    Circe KeyEncoder/KeyDecoder for Enum (#219)

---
+++ enumeratum-circe/src/test/scala/enumeratum/CirceKeySpec.scala
@@ -0,0 +1,28 @@
+package enumeratum
+
+import org.scalatest.{FunSpec, Matchers}
+import cats.syntax.either._
+import io.circe.Json
+import io.circe.syntax._
+
+class CirceKeySpec extends FunSpec with Matchers {
+  describe("to JSON") {
+    it("should work") {
+      Map(ShirtSize.Small -> 5, ShirtSize.Large -> 10).asJson shouldBe Json.obj(
+        "Small" -> 5.asJson,
+        "Large" -> 10.asJson
+      )
+    }
+  }
+
+  describe("from JSON") {
+    it("should work") {
+      Json
+        .obj(
+          "Medium" -> 100.asJson,
+          "Large"  -> 15.asJson
+        )
+        .as[Map[ShirtSize, Int]] shouldBe Map(ShirtSize.Medium -> 100, ShirtSize.Large -> 15).asRight
+    }
+  }
+}
