f483d401c0697224307706037f64cbab9317da8d
lloydmeta <lloydmeta@gmail.com>
2021-07-22 15:54:58 +0900

    format this one file???

--- enumeratum-core/src/main/scala/enumeratum/Enum.scala
+++ enumeratum-core/src/main/scala/enumeratum/Enum.scala
@@ -165,7 +165,7 @@ trait Enum[A <: EnumEntry] {
     *   the member you want to check the index of
     * @return
     *   the index of the first element of values that is equal (as determined by ==) to member, or
-    *   -1, if none exists.
+    * -1, if none exists.
     */
   def indexOf(member: A): Int = valuesToIndex.getOrElse(member, -1)
 
