8bfe7679fac173609290477548918cafafda2a9a
Lloyd <lloydmeta@users.noreply.github.com>
2022-12-04 03:12:24 +0900

    Scala 3 release (#354)
    * Bump versions for release
    
    * Bump to SNAPSHOT verions
    
    Signed-off-by: Lloyd <lloydmeta@gmail.com>

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,13 +1,13 @@
 object Versions {
 
   object Core {
-    val stable = "1.7.0"
+    val stable = "1.7.1"
     val head   = "1.7.2-SNAPSHOT"
   }
 
   object Macros {
-    val stable = "1.6.1"
-    val head   = "1.6.2-SNAPSHOT"
+    val stable = "1.6.2"
+    val head   = "1.6.3-SNAPSHOT"
   }
 
 }
