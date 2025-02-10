70674b48b8b09ace21e69462d03832e2751740c4
Mike Limansky <mike.limansky@gmail.com>
2025-02-10 21:57:28 +0300

    Use timap instead of imap

--- enumeratum-doobie/src/main/scala/enumeratum/Doobie.scala
+++ enumeratum-doobie/src/main/scala/enumeratum/Doobie.scala
@@ -8,5 +8,5 @@ object Doobie {
   /** Returns an Encoder for the given enum
     */
   def meta[A <: EnumEntry](@deprecatedName(Symbol("enum")) e: Enum[A]): Meta[A] =
-    Meta[String].imap(e.withName)(_.entryName)
+    Meta[String].timap(e.withName)(_.entryName)
 }
