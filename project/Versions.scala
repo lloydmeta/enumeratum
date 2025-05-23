f2c00e9116635646e70ccb665d1e870dbf540e6a
lloydmeta <lloydmeta@gmail.com>
2025-05-23 21:39:59 +0900

    Publish 1.9.0
    Signed-off-by: lloydmeta <lloydmeta@gmail.com>

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,13 +1,13 @@
 object Versions {
 
   object Core {
-    val stable = "1.7.6"
-    val head   = "1.7.7-SNAPSHOT"
+    val stable = "1.9.0"
+    val head   = "1.9.1-SNAPSHOT"
   }
 
   object Macros {
-    val stable = "1.7.5"
-    val head   = "1.7.6-SNAPSHOT"
+    val stable = "1.9.0"
+    val head   = "1.9.1-SNAPSHOT"
   }
 
 }
