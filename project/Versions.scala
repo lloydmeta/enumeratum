127609380fda0b796bbe9debf3cf218cf89ebccb
lloydmeta <lloydmeta@gmail.com>
2026-02-01 18:24:09 +0900

    1.9.5-SNAPSHOT begins
    Signed-off-by: lloydmeta <lloydmeta@gmail.com>

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,12 +2,12 @@ object Versions {
 
   object Core {
     val stable = "1.9.4"
-    val head   = "1.9.4-SNAPSHOT"
+    val head   = "1.9.5-SNAPSHOT"
   }
 
   object Macros {
     val stable = "1.9.4"
-    val head   = "1.9.4-SNAPSHOT"
+    val head   = "1.9.5-SNAPSHOT"
   }
 
 }
