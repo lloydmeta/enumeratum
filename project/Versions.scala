423dfe38bcde6214ccc02dcf1841a16e58a3ec5b
lloydmeta <lloydmeta@gmail.com>
2026-03-07 18:37:12 +0900

    1.9.6 release
    Signed-off-by: lloydmeta <lloydmeta@gmail.com>

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,12 +1,12 @@
 object Versions {
 
   object Core {
-    val stable = "1.9.5"
+    val stable = "1.9.6"
     val head   = "1.9.6-SNAPSHOT"
   }
 
   object Macros {
-    val stable = "1.9.5"
+    val stable = "1.9.6"
     val head   = "1.9.6-SNAPSHOT"
   }
 
