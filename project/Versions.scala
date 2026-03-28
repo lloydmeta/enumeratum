2bc71b461197eae227cceafea68d5cea3e00ddf0
lloydmeta <lloydmeta@gmail.com>
2026-03-28 23:10:28 +0900

    enumeratum 1.9.7 release
    Signed-off-by: lloydmeta <lloydmeta@gmail.com>

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,12 +1,12 @@
 object Versions {
 
   object Core {
-    val stable = "1.9.6"
+    val stable = "1.9.7"
     val head   = "1.9.7-SNAPSHOT"
   }
 
   object Macros {
-    val stable = "1.9.6"
+    val stable = "1.9.7"
     val head   = "1.9.7-SNAPSHOT"
   }
 
